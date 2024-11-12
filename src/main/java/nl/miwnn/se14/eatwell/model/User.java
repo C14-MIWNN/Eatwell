a15a7708c62463e5babb5dd2fd7d3715029ac836
Bart Molenaars <bartmolenaars12@gmail.com>
2024-11-12 15:05:53 +0100

    updated user class

--- src/main/java/nl/miwnn/se14/eatwell/model/User.java
+++ src/main/java/nl/miwnn/se14/eatwell/model/User.java
@@ -4,5 +4,42 @@ package nl.miwnn.se14.eatwell.model;
  * @author Bart Molenaars
  * Purpose for the class
  */
+
 public class User {
+    private Long userId;
+    private String username;
+    private String emailAdress;
+    private String password;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmailAdress() {
+        return emailAdress;
+    }
+
+    public void setEmailAdress(String emailAdress) {
+        this.emailAdress = emailAdress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
