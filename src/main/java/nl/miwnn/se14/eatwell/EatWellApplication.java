6b8c08f7ca835de2a48c8a91157a0513021e55f3
Bart Molenaars <bartmolenaars12@gmail.com>
2024-11-12 14:21:12 +0100

    Spring initialize

---
+++ src/main/java/nl/miwnn/se14/eatwell/EatWellApplication.java
@@ -0,0 +1,13 @@
+package nl.miwnn.se14.eatwell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatWellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EatWellApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/miwnn/se14/eatwell/EatWellApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.miwnn.se14.eatwell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatWellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
