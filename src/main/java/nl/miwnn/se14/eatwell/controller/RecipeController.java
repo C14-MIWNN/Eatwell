3d8b45a910fea8e290388b4a8ebc112eb5c6b541
Furkan Altay <frknlty4@gmail.com>
2024-11-14 13:54:28 +0100

    added general.html

--- src/main/java/nl/miwnn/se14/eatwell/controller/RecipeController.java
+++ src/main/java/nl/miwnn/se14/eatwell/controller/RecipeController.java
@@ -4,6 +4,7 @@ import nl.miwnn.se14.eatwell.repositories.RecipeRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * @author Furkan Altay
