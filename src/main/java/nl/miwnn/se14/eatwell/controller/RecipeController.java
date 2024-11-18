ffee68602273bdd8e8911f567275ae1963780a6f
Furkan Altay <frknlty4@gmail.com>
2024-11-18 16:16:23 +0100

    edited RecipeController

--- src/main/java/nl/miwnn/se14/eatwell/controller/RecipeController.java
+++ src/main/java/nl/miwnn/se14/eatwell/controller/RecipeController.java
@@ -1,9 +1,14 @@
 package nl.miwnn.se14.eatwell.controller;
 
+import nl.miwnn.se14.eatwell.model.Recipe;
+import nl.miwnn.se14.eatwell.repositories.CategoryRepository;
 import nl.miwnn.se14.eatwell.repositories.RecipeRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -13,15 +18,35 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class RecipeController {
     private final RecipeRepository recipeRepository;
+    private final CategoryRepository categoryRepository;
 
 
-    public RecipeController(RecipeRepository recipeRepository) {
+    public RecipeController(RecipeRepository recipeRepository, CategoryRepository categoryRepository) {
         this.recipeRepository = recipeRepository;
+        this.categoryRepository = categoryRepository;
     }
 
     @GetMapping({"/","/recipe/overview"})
     private String showRecipeOverview(Model datamodel) {
         datamodel.addAttribute("allRecipes", recipeRepository.findAll());
+        return "recipeOverview";
+    }
+
+    @GetMapping({"/recipe/new"})
+    private String showRecipeCreation(Model datamodel) {
+        datamodel.addAttribute("newRecipe", new Recipe());
+        datamodel.addAttribute("allCategories", categoryRepository.findAll());
+
+        return "recipeCreation";
+    }
+
+    @PostMapping({"/recipe/new"})
+    private String saveOrUpdateRecipe(@ModelAttribute("newRecipe") Recipe recipe, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "recipe/new";
+        }
+
+        recipeRepository.save(recipe);
         return "recipeOverview";
     }
 
