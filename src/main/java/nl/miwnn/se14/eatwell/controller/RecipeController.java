fb656189ea74766eb05320527696f3ab25c72757
Furkan Altay <frknlty4@gmail.com>
2024-11-19 13:35:55 +0100

    Edited recipeCreation en RecipeController

--- src/main/java/nl/miwnn/se14/eatwell/controller/RecipeController.java
+++ src/main/java/nl/miwnn/se14/eatwell/controller/RecipeController.java
@@ -10,10 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-/**
- * @author Furkan Altay
- * Purpose for the class
- */
+
 @Controller
 public class RecipeController {
     private final RecipeRepository recipeRepository;
